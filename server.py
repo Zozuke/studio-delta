ee773d12c9b5447f6312c5d156937f20c9f9214f
Zusita <118792001+Zozuke@users.noreply.github.com>
2025-02-03 12:44:58 -0600

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import smtplib
 import random
+import os
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,9 +11,9 @@ CORS(app)  # Permite que cualquier página web pueda acceder a este servidor
 def generar_codigo():
     return str(random.randint(100000, 999999))
 
-# Configuración del servidor de correo
-EMAIL = "deltafx.studio@gmail.com.pro"  # Tu correo
-PASSWORD = "TU_CONTRASEÑA"  # Aquí debes poner la contraseña de tu correo
+# Configuración del servidor de correo usando variables de entorno
+EMAIL = os.environ.get("EMAIL")
+PASSWORD = os.environ.get("PASSWORD")
 
 def enviar_correo(destinatario, codigo):
     try:
@@ -38,4 +39,5 @@ def suscribirse():
     return jsonify({"mensaje": "Correo enviado", "codigo": codigo})
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 10000))  # Render asigna un puerto automáticamente
+    app.run(debug=True, host="0.0.0.0", port=port)
